85f343d331591f10cf6cdb975fe5993968bbf8f9
Ayush16coder <dongareayush17@gmail.com>
2026-05-20 18:13:33 +0530

    feat(ui): set default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className={inter.variable}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
